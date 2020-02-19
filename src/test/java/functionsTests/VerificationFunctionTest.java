17e167863189f57258dd363cf63049f042429e4f
marwin.torrez <marwin.torrez@truextend.com>
2020-02-19 13:15:03 -0400

    [develop] Changing all variables to camelCase

--- src/test/java/functionsTests/VerificationFunctionTest.java
+++ src/test/java/functionsTests/VerificationFunctionTest.java
@@ -15,22 +15,22 @@ public class VerificationFunctionTest {
 	public void testExample1() {
 		List<Student> studentList = new ArrayList<Student>();
 		List<Classroom> classroomList = new ArrayList<Classroom>();
-		Student john_student = new Student("John Wilson", 34.069149, -118.442639);
-		Student jane_student = new Student("Jane Graham", 34.069601, -118.441862);
-		Student pam_student = new Student("Pam Bam", 34.071513, -118.441181);
+		Student johnStudent = new Student("John Wilson", 34.069149, -118.442639);
+		Student janeStudent = new Student("Jane Graham", 34.069601, -118.441862);
+		Student pamStudent = new Student("Pam Bam", 34.071513, -118.441181);
 		classroomList.add(new Classroom("Principles of computational geo-location analysis", 34.069140, -118.442689));
 		classroomList.add(new Classroom("Sedimentary Petrology", 34.069585, -118.441878));
 		classroomList.add(new Classroom("Introductory Psychobiology", 34.069742, -118.441312));
 		classroomList.add(new Classroom("Art of Listening", 34.070223, -118.440193));
 		classroomList.add(new Classroom("Art Hitory", 34.071528, -118.441211));
-		studentList.add(john_student);
-		studentList.add(jane_student);
-		studentList.add(pam_student);
+		studentList.add(johnStudent);
+		studentList.add(janeStudent);
+		studentList.add(pamStudent);
 		List<Student> studentFoundList = VerificationFunctions.studentsInClasses(studentList, classroomList);
 		List<Student> expectedStudentFoundList = new ArrayList<Student>();
-		expectedStudentFoundList.add(john_student);
-		expectedStudentFoundList.add(jane_student);
-		expectedStudentFoundList.add(pam_student);
+		expectedStudentFoundList.add(johnStudent);
+		expectedStudentFoundList.add(janeStudent);
+		expectedStudentFoundList.add(pamStudent);
 		assertEquals(studentFoundList,expectedStudentFoundList);
 	}
 	
@@ -38,20 +38,20 @@ public class VerificationFunctionTest {
 	public void testExample2() {
 		List<Student> studentList = new ArrayList<Student>();
 		List<Classroom> classroomList = new ArrayList<Classroom>();
-		Student john_student = new Student("John Wilson", 34.069849, -118.443539);
-		Student jane_student = new Student("Jane Graham", 34.069901, -118.441562);
-		Student pam_student = new Student("Pam Bam", 34.071523, -118.441171);
+		Student johnStudent = new Student("John Wilson", 34.069849, -118.443539);
+		Student janeStudent = new Student("Jane Graham", 34.069901, -118.441562);
+		Student pamStudent = new Student("Pam Bam", 34.071523, -118.441171);
 		classroomList.add(new Classroom("Principles of computational geo-location analysis", 34.069140, -118.442689));
 		classroomList.add(new Classroom("Sedimentary Petrology", 34.069585, -118.441878));
 		classroomList.add(new Classroom("Introductory Psychobiology", 34.069742, -118.441312));
 		classroomList.add(new Classroom("Art of Listening", 34.070223, -118.440193));
 		classroomList.add(new Classroom("Art Hitory", 34.071528, -118.441211));
-		studentList.add(john_student);
-		studentList.add(jane_student);
-		studentList.add(pam_student);
+		studentList.add(johnStudent);
+		studentList.add(janeStudent);
+		studentList.add(pamStudent);
 		List<Student> studentFoundList = VerificationFunctions.studentsInClasses(studentList, classroomList);
 		List<Student> expectedStudentFoundList = new ArrayList<Student>();
-		expectedStudentFoundList.add(pam_student);
+		expectedStudentFoundList.add(pamStudent);
 		assertEquals(studentFoundList,expectedStudentFoundList);
 	}
 	
@@ -59,17 +59,17 @@ public class VerificationFunctionTest {
 	public void testAllStudentsOutOfClasses() {
 		List<Student> studentList = new ArrayList<Student>();
 		List<Classroom> classroomList = new ArrayList<Classroom>();
-		Student john_student = new Student("John Wilson", 34.069849, -118.443539);
-		Student jane_student = new Student("Jane Graham", 34.069901, -118.441562);
-		Student pam_student = new Student("Pam Bam", 34.071633, -118.441171);
+		Student johnStudent = new Student("John Wilson", 34.069849, -118.443539);
+		Student janeStudent = new Student("Jane Graham", 34.069901, -118.441562);
+		Student pamStudent = new Student("Pam Bam", 34.071633, -118.441171);
 		classroomList.add(new Classroom("Principles of computational geo-location analysis", 34.069140, -118.442689));
 		classroomList.add(new Classroom("Sedimentary Petrology", 34.069585, -118.441878));
 		classroomList.add(new Classroom("Introductory Psychobiology", 34.069742, -118.441312));
 		classroomList.add(new Classroom("Art of Listening", 34.070223, -118.440193));
 		classroomList.add(new Classroom("Art Hitory", 34.071528, -118.441211));
-		studentList.add(john_student);
-		studentList.add(jane_student);
-		studentList.add(pam_student);
+		studentList.add(johnStudent);
+		studentList.add(janeStudent);
+		studentList.add(pamStudent);
 		List<Student> studentFoundList = VerificationFunctions.studentsInClasses(studentList, classroomList);
 		List<Student> expectedStudentFoundList = new ArrayList<Student>();
 		assertEquals(studentFoundList,expectedStudentFoundList);
